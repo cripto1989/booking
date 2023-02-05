9b41294f8b347b289408e400157ea71ba22f7a60
Juan Herrera <cripto1989@gmail.com>
2023-02-05 15:25:21 -0600

    feat: Add endpoint for canceling a booking

--- core/rooms/api/urls.py
+++ core/rooms/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookingView, EventView, RoomView
+from .views import BookingView, EventView, RoomView, CancelBookingView
 
 urlpatterns = [    
     path("room/", RoomView.as_view(), name="create_room"),
     path("event/", EventView.as_view(), name="create_event"),
+    path("booking/cancel/<uuid:id>/", CancelBookingView.as_view(), name="cancel_booking"),
     path("booking/", BookingView.as_view(), name="create_booking"),
 ]
--- core/rooms/api/views.py
+++ core/rooms/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView
 from .serializers import BookingSerializer, EventSerializer, RoomSerializer
 from rooms.models import Booking, Event, Room
 
@@ -16,3 +16,9 @@ class EventView(CreateAPIView):
 class BookingView(CreateAPIView):
     serializer_class = BookingSerializer
     queryset = Booking.objects.all()
+
+
+class CancelBookingView(DestroyAPIView):
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
+    lookup_field = "id"
