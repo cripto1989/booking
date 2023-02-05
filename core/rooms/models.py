16c2d788d4744a7272ec6dc9b2a7dfa0f7fbfd56
Juan Herrera <cripto1989@gmail.com>
2023-02-05 13:49:39 -0600

    feat: Add model Event

--- core/rooms/models.py
+++ core/rooms/models.py
@@ -1,4 +1,5 @@
 import uuid
+from enum import Enum
 from django.db import models
 from model_utils.models import TimeStampedModel
 
@@ -8,4 +9,22 @@ class Room(TimeStampedModel):
     capacity = models.SmallIntegerField(default=1)
 
     def __str__(self) -> str:
-        return f"Room {self.id} with capacity {self.capacity}"
+        return f"{self.id} with capacity {self.capacity}"
+
+
+class EventType(Enum):
+    PUBLIC = "Public"
+    PRIVATE = "Private"
+
+    @classmethod
+    def choices(cls):
+        return [(type_event.name, type_event.value) for type_event in cls]
+
+
+class Event(TimeStampedModel):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    models.CharField(max_length=10, choices=EventType.choices())
+
+    def __str__(self) -> str:
+        return f"{self.id} in room {self.room.id}"
