22efee4a7089a15105d8e4af5440e1da27888b35
Juan Herrera <cripto1989@gmail.com>
2023-02-05 15:56:56 -0600

    feat: Add endpoint for deleting a room

--- core/rooms/api/urls.py
+++ core/rooms/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookingView, EventView, RoomView, CancelBookingView
+from .views import BookingView, EventView, RoomView, CancelBookingView, DeleteRoomView
 
 urlpatterns = [    
+    path("room/delete/<uuid:id>/", DeleteRoomView.as_view(), name="delete_room"),
     path("room/", RoomView.as_view(), name="create_room"),
     path("event/", EventView.as_view(), name="create_event"),
     path("booking/cancel/<uuid:id>/", CancelBookingView.as_view(), name="cancel_booking"),
--- core/rooms/api/views.py
+++ core/rooms/api/views.py
@@ -1,4 +1,9 @@
+from django.http import Http404
 from rest_framework.generics import CreateAPIView, DestroyAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
 from .serializers import BookingSerializer, EventSerializer, RoomSerializer
 from rooms.models import Booking, Event, Room
 
@@ -6,7 +11,18 @@ from rooms.models import Booking, Event, Room
 class RoomView(CreateAPIView):
     serializer_class = RoomSerializer
     queryset = Room.objects.all()
+     
 
+class DeleteRoomView(DestroyAPIView):
+    serializer_class = RoomSerializer
+    queryset = Room.objects.all()    
+    lookup_field = "id"
+
+    def destroy(self, request, *args, **kwargs):
+        room = self.get_object()
+        if room.event_set.all():
+            return Response({"message": "There is at least an event related to this room"}, status=status.HTTP_400_BAD_REQUEST)
+        return super().destroy(request, *args, **kwargs)
 
 class EventView(CreateAPIView):
     serializer_class = EventSerializer
